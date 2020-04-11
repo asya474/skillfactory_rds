12b870a5eef2e2a9a3f095239c78f6ca070cdace
asya474 <51288093+asya474@users.noreply.github.com>
2020-04-11 17:44:27 +0700

    project 0

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,13 @@
- print("Hello world")
+import numpy as np
+predict = np.random.randint(1,101)
+i=0
+left = 1
+right = 100
+while True:
+    i+=1
+    current = (left+right)//2
+    if current == predict:
+        print('Вы угадали с {} попытки!'.format(i))
+        break
+    elif current < predict:left = current + 1
+    else:right = current - 1
